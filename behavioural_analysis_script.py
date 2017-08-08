c6c2406ab2c7c7f6a3943443254e414962eda14c
elenahindinger <elena.hw@gmx.at>
2017-08-08 14:16:05 +0200

    Allows to make a randomized list of conditions for a 48 or 96 well plate set up and saves it as a csv file to a specified output directory.

--- behavioural_analysis_script.py
+++ behavioural_analysis_script.py
@@ -1,21 +1,32 @@
 from __future__ import division
 import os
+import pandas as pd
+import numpy as np
+import natsort as nat
+from matplotlib import pyplot as plt
+import matplotlib.ticker as ticker
+from scipy import stats
+import seaborn as sns
 import behavioural_analysis_function as ba
 
 __author__ = 'Elena Maria Daniela Hindinger'
 
-
 # MODULAR
-parent_folder = r'E:\24h trial analysis\pilot experiments\trial1 pigmented\NEW ANALYSIS'
-look_up_table = os.path.join(parent_folder, 'randomization list', '17-04-17_DanioVision_matched_list_gr_het_longterm.csv')
-name_of_experiment = 'longterm_pilot'
-names_of_groups = ['gr', 'het']
+parent_folder = r'I:\Elena H\BEHAVIOUR\Spontaneous Locomotion Experiments\DanioVision Custom Tracker Output\longterm_H2B\DanioVision tracked'
+look_up_table = os.path.join(parent_folder, 'randomization list', '28-06-17_DanioVision_matched_list_H2B_gr_het_longterm.csv')
+name_of_experiment = 'longterm_H2B'
+#==============================================================================
+# names_of_groups = ['gr dmso', 'gr 20 uM', 'gr 50 uM', 'gr 100 uM',
+#                    'het dmso', 'het 20 uM', 'het 50 uM', 'het 100 uM']
+# concentrations = ['dmso', '20 uM', '50 uM', '100 uM']
+#==============================================================================
+names_of_groups = ['gr H2B', 'het H2B']
 concentrations = ['dmso']
 number_of_groups = 2
 n_per_group = int(96 / number_of_groups)
 type_of_experiment = 'longterm'  # longterm or acute
 length_of_experiment = 24  # in hours
-day1 = 4
+day1 = 7
 
 ba.analysis(parent_folder, look_up_table, name_of_experiment, names_of_groups,
             concentrations, number_of_groups, n_per_group, length_of_experiment,
